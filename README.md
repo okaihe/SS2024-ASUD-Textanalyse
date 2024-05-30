f431b3506cc0bb179978ff919c4a89012ff875be
okaihe <kai.herbst@online.de>
2024-05-31 01:41:10 +0200

    Add ntv-lambda

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Analyse von semi- und unstrukturierten Daten 🌍
 
 Dieses Repository beinhaltet den Code, der im Rahmen der "Zusäztlichen Beteiligung" des Moduls "Analyse von semi- und unstrukturierten Daten" des Sommersemestern 2024 an der FOM erstellt wurde. Hierin enthalten sind `Jupyter-Notebooks`, die unter anderem die Generierung und Verarbeitung der Rohdaten sowie eine Sentiment-Analyse und ein Topic-Modelling der verarbeiteten Daten beinhalten.
+
+Analysiert werden die Artikel des Nachrichtensenders N-TV.
+
+## Inhalte 📕
+
+Das Repository ist wie folgt aufgebaut:
+
+- `ntv-data/` Hierin sind die Rohdaten enthalten. Die Artikel sind zunächst nach dem jeweiligen Datum und anschließend nach deren Kategorie sortiert.
+- `ntv-lambda/` Hierin ist die Lambda-Funktion zum täglichen Abrufen der Artikel enthalten.
+- `1-Datengenerierung.ipynb` Der Code, zum Erstellen der `data.csv`, die als Grundlage der Analyse dient.
+- `2-Datenanalyse.ipynb` Die Analyse der Nachrichtenartiekl.
---
+++ ntv-lambda/index.mjs
@@ -0,0 +1,28 @@
+import helper from "./helper.mjs";
+import categories from "./categories.mjs";
+import S3Uploader from "./s3Helper.mjs";
+
+export const handler = async (event, context) => {
+    const s3Uploader = new S3Uploader();
+    const bucket = "ntv-data";
+    const baseKey = helper.getYesterdaysDate().toISOString().split("T")[0];
+    for (let category of categories) {
+        let { links, rss } = await helper.getLinksOfYesterdaysArticelsAndRssObjectOf(category);
+        if (links.length == 0) {
+            continue;
+        }
+        let rssKey = baseKey + "/" + category + "/rss.json";
+        let rssJSONString = JSON.stringify(rss);
+        await s3Uploader.uploadContent(bucket, rssKey, rssJSONString);
+        for (let link of links) {
+            let articleKey = baseKey + "/" + category + "/" + helper.getFileNameFromLink(link);
+            let htmlContent = await helper.downloadContentFrom(link);
+            let cleanedHTML = helper.cleanHTMLContent(htmlContent);
+            await s3Uploader.uploadContent(bucket, articleKey, cleanedHTML);
+        }
+    }
+    return {
+        statusCode: 200,
+        body: JSON.stringify("Done!"),
+    };
+};
